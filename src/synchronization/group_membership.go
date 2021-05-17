1c1d7b0c2a8b429164a3da532417d6dc9952e96c
CVIllustrious <muchi@ucsd.edu>
2021-05-17 22:45:50 +0000

    join & listen in group membership

--- src/synchronization/group_membership.go
+++ src/synchronization/group_membership.go
@@ -1,1 +1,22 @@
 package synchronization
+
+import (
+	"github.com/go-zookeeper/zk"
+)
+
+func join (conn *zk.Conn, groupPath string, selfName string) (string, error){
+	s, e := conn.CreateProtectedEphemeralSequential(groupPath + "/" + selfName, []byte(selfName), zk.WorldACL(zk.PermAll))
+	return s, e
+}
+
+func listen (conn *zk.Conn, groupPath string, callback func([]string)) error{
+	children, _, event, e := conn.ChildrenW(groupPath)
+	for ;; {
+		if zk.EventNodeChildrenChanged  == (<-event).Type  {
+			children, _, event, e = conn.ChildrenW(groupPath)
+
+			go callback(children)
+		}
+	}
+	return e
+}
