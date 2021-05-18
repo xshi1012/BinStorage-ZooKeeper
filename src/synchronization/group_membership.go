c8e6078dbb2d230f77b9d99e70813c937918f615
xshi1012 <xshi@ucsd.edu>
2021-05-17 17:17:34 -0700

    makefile

--- src/synchronization/group_membership.go
+++ src/synchronization/group_membership.go
@@ -4,12 +4,12 @@ import (
 	"github.com/go-zookeeper/zk"
 )
 
-func join (conn *zk.Conn, groupPath string, selfName string) (string, error){
+func join(conn *zk.Conn, groupPath string, selfName string) (string, error){
 	s, e := conn.CreateProtectedEphemeralSequential(groupPath + "/" + selfName, []byte(selfName), zk.WorldACL(zk.PermAll))
 	return s, e
 }
 
-func listen (conn *zk.Conn, groupPath string, callback func([]string)) error{
+func listen(conn *zk.Conn, groupPath string, callback func([]string)) error{
 	children, _, event, e := conn.ChildrenW(groupPath)
 	for ;; {
 		if zk.EventNodeChildrenChanged  == (<-event).Type  {
