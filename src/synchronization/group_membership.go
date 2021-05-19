a44f4cc5d4f700ed2fefba957f3d7f1fd195bc23
xshi1012 <xshi@ucsd.edu>
2021-05-18 22:11:52 -0700

    use struct for group membership

--- src/synchronization/group_membership.go
+++ src/synchronization/group_membership.go
@@ -2,21 +2,71 @@ package synchronization
 
 import (
 	"github.com/go-zookeeper/zk"
+	"strings"
 )
 
-func join(conn *zk.Conn, groupPath string, selfName string) (string, error){
-	s, e := conn.CreateProtectedEphemeralSequential(groupPath + "/" + selfName, []byte(selfName), zk.WorldACL(zk.PermAll))
+type GroupMember struct {
+	groupPath string
+	selfName string
+	conn *zk.Conn
+}
+
+func NewGroupMember(conn *zk.Conn, groupPath string, selfName string) *GroupMember {
+	g := new(GroupMember)
+	g.conn = conn
+	g.groupPath = groupPath
+	g.selfName = selfName
+
+	return g
+}
+
+func (self *GroupMember) Join() (string, error) {
+	exist, _, e := self.conn.Exists(self.groupPath)
+	if e != nil {
+		return "", e
+	} else if !exist {
+		_, e = createRecursive(self.conn, self.groupPath, []byte{}, 0, zk.WorldACL(zk.PermAll))
+		if e != nil {
+			return "", e
+		}
+	}
+
+	s, e := CreateProtectedEphemeral(self.conn, self.groupPath + "/" + self.selfName, []byte{}, zk.WorldACL(zk.PermAll))
+
 	return s, e
 }
 
-func listen(conn *zk.Conn, groupPath string, callback func([]string)) error{
-	children, _, event, e := conn.ChildrenW(groupPath)
+func (self *GroupMember) Listen(callback func([]string)) error {
+	children, _, event, e := self.conn.ChildrenW(self.groupPath)
+	if e != nil {
+		return e
+	}
+
 	for ;; {
 		if zk.EventNodeChildrenChanged  == (<-event).Type  {
-			children, _, event, e = conn.ChildrenW(groupPath)
+			children, _, event, e = self.conn.ChildrenW(self.groupPath)
+			if e != nil {
+				return e
+			}
+
+			for i, v := range children {
+				children[i] = strings.Split(v, "-")[1]
+			}
 
 			go callback(children)
 		}
 	}
-	return e
+}
+
+func (self *GroupMember) GetCurrentMembers() ([]string, error) {
+	children, _, e := self.conn.Children(self.groupPath)
+	if e != nil {
+		return nil, e
+	}
+
+	for i, v := range children {
+		children[i] = strings.Split(v, "-")[1]
+	}
+
+	return children, e
 }
--- src/synchronization/helpers.go
+++ src/synchronization/helpers.go
@@ -1,10 +1,16 @@
 package synchronization
 
 import (
+	"crypto/rand"
+	"fmt"
 	"github.com/go-zookeeper/zk"
+	"io"
 	"strings"
 )
 
+const (
+	protectedPrefix = "_c_"
+)
 func createRecursive(conn *zk.Conn, path string, data []byte, flags int32, acl []zk.ACL) (string, error) {
 	name, e := conn.Create(path, data, flags, acl)
 	if e == zk.ErrNoNode {
@@ -30,4 +36,46 @@ func createRecursive(conn *zk.Conn, path string, data []byte, flags int32, acl [
 	}
 
 	return name, e
+}
+
+func CreateProtectedEphemeral(conn *zk.Conn, path string, data []byte, acl []zk.ACL) (string, error) {
+	var guid [16]byte
+	_, e := io.ReadFull(rand.Reader, guid[:16])
+	if e != nil {
+		return "", e
+	}
+	guidStr := fmt.Sprintf("%x", guid)
+
+	parts := strings.Split(path, "/")
+	parts[len(parts)-1] = fmt.Sprintf("%s%s-%s", protectedPrefix, guidStr, parts[len(parts)-1])
+	rootPath := strings.Join(parts[:len(parts)-1], "/")
+	protectedPath := strings.Join(parts, "/")
+
+	var newPath string
+
+	for i := 0; i < 3; i++ {
+		newPath, e = conn.Create(protectedPath, data, zk.FlagEphemeral, acl)
+		switch e {
+		case zk.ErrSessionExpired:
+		case zk.ErrConnectionClosed:
+			children, _, e := conn.Children(rootPath)
+			if e != nil {
+				return "", e
+			}
+
+			for _, p := range children {
+				parts := strings.Split(p, "/")
+				if pth := parts[len(parts)-1]; strings.HasPrefix(pth, protectedPrefix) {
+					if g := pth[len(protectedPrefix) : len(protectedPrefix)+32]; g == guidStr {
+						return rootPath + "/" + p, nil
+					}
+				}
+			}
+		case nil:
+			return newPath, e
+		default:
+			return "", e
+		}
+	}
+	return "", e
 }
