2b04cf82472766d17b27e55a4a30a11a78e3906f
CVIllustrious <muchi@ucsd.edu>
2021-05-25 21:26:54 +0000

    simple back without callback

--- src/bin_back/bin_back.go
+++ src/bin_back/bin_back.go
@@ -1,15 +1,69 @@
 package bin_back
 
-import "BinStorageZK/src/bin_back/bin_config"
+import (
+	"BinStorageZK/src/bin_back/bin_config"
+	"BinStorageZK/src/synchronization"
+	"net"
+	"net/http"
+	"net/rpc"
+	"github.com/go-zookeeper/zk"
+	"time"
+)
+
+const (
+	GroupPath = "back"
+)
 
 type BinBack struct {
-
+	config *bin_config.BackConfig
 }
 
 func NewBinBack(b *bin_config.BackConfig) *BinBack {
-	panic("todo")
+	binBack := new(BinBack)
+	binBack.config = b
+	return binBack
 }
 
 func (self *BinBack) Run() error {
-	panic("todo")
+	server := rpc.NewServer()
+	server.RegisterName("Storage", self.config.Store)
+
+	l, e := net.Listen("tcp", self.config.Addr)
+
+	if e != nil {
+		if self.config.Ready != nil {
+			self.config.Ready <- false
+		}
+		return e
+	}
+
+	conn, _, e := zk.Connect(self.config.Keepers, time.Second)
+	if e != nil {
+		if self.config.Ready != nil {
+			self.config.Ready <- false
+		}
+		return e
+	}
+
+	if self.config.Ready != nil {
+		self.config.Ready <- true
+	}
+
+	groupMember := synchronization.NewGroupMember(conn, GroupPath, self.config.Addr)
+	_, e = groupMember.Join()
+	go groupMember.Listen(func([]string){
+		
+	})
+
+	e = http.Serve(l, server)
+
+	if e != nil {
+		if self.config.Ready != nil {
+			self.config.Ready <- false
+		}
+		return e
+	}
+
+
+	return e
 }
